943ae34d2c0d86dad353887017b811c6b47e5229
Graham Coulby <gcoulby@googlemail.com>
2021-03-19 13:25:48 +0000

    first commit

--- README.md
+++ README.md
@@ -1,12 +1,49 @@
-# Getting Started with Create React App
+# iOS Health Data Parser
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is application parses the XML data exported from iOS Health app and creates CSV files for the chosen health categories.
+
+This application was developed to support research funded by Northumbria University and the European Regional Development Fund’s Intensive Industrial Innovation Programme as part of doctoral PhD programme. The sponsoring Small to Medium Enterprise for this programme was Ryder Architecture and it was delivered through Northumbria University.
+
+
+
+[![European Regional Development Fund logo](http://localhost:3000/static/media/erdf.dd68eb2a.svg)](https://ec.europa.eu/regional_policy/en/funding/erdf/)			[![Northumbria University Logo](http://localhost:3000/static/media/northumbria.9f973ad5.svg)](https://www.northumbria.ac.uk/)			[![Ryder Architecture Logo](http://localhost:3000/static/media/ryder.0a9e1d7e.svg)](https://ryderarchitecture.com/)
+
+
+
+## Usage
+
+This application is available for use without installation via GitHub pages. This application can be found here:
+
+
+
+### Instructions
+
+This application parses the `export.xml` file obtained from iOS health app. To obtain the `export.xml` follow the instructions provided by Apple:
+
+https://support.apple.com/en-gb/guide/iphone/iph27f6325b2/ios
+
+> **The `export.xml` file will contain personal information that can be used to identify the user of the application. It will also contain all data captured by the application. This application will remove the personal data and allow you to control which data is exported, so you can share the sanitised data instead**
+
+Once you have obtained your `export.xml` file, simply drag it into the box inside  app. Once the file has finished parsing, a list of health data categories will appear at the bottom of the screen. By default the toggle switches for each box are disabled. Click the health data categories you wish to export then press the `Save File(s)` button. This will create a compressed zip archive that contains a separate Comma Separated Value (CSV) file for each category of health data. 
+
+
+
+## Installation
+
+To install this application run the following command to clone the repository and change the active directory to the newly cloned repo.
+
+```bash
+$ git clone https://github.com/gcoulby/apple-health-data-parser.git
+$ cd apple-health-data-parser
+```
+
+
 
 ## Available Scripts
 
 In the project directory, you can run:
 
-### `yarn start`
+### `npm start`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
@@ -14,12 +51,7 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
 
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
+### `npm run build`
 
 Builds the app for production to the `build` folder.\
 It correctly bundles React in production mode and optimizes the build for the best performance.
@@ -28,43 +60,3 @@ The build is minified and the filenames include the hashes.\
 Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,317 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "xml-js/dist/xml-js.js";
+import JSZip from "jszip/dist/jszip.js";
+import "./App.css";
+import erdf from "./img/erdf.svg";
+import northumbria from "./img/northumbria.svg";
+import ryder from "./img/ryder.svg";
+import React, { Component } from "react";
+class App extends Component {
+  state = {
+    recordFields: [
+      "sourceName",
+      "sourceVersion",
+      "device",
+      "type",
+      "unit",
+      "creationDate",
+      "startDate",
+      "endDate",
+      "value",
+    ],
+    status: (
+      <span>
+        <strong>No File Loaded.</strong> Drag a file into the box below to begin
+      </span>
+    ),
+    alertLevel: "secondary",
+    fileContents: "",
+    records: [],
+    categories: [],
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleOnDragOver = (evt) => {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = "copy";
+  };
+
+  handleOnDrop = (evt) => {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var files = evt.dataTransfer.files;
+    this.readBlob(files[0]);
+    this.setState({
+      status: (
+        <span>
+          Parsing data from <strong>{files[0].name}</strong>.
+        </span>
+      ),
+    });
+    this.setState({ alertLevel: "info" });
+  };
+
+  handleOnCheck = (evt, id) => {
+    const categories = this.state.categories.map((c) => {
+      if (c.name === id) c.checked = evt.target.checked;
+      return c;
+    });
+    this.setState({ categories });
+  };
+
+  handleDownloadClick = () => {
+    this.createZipFile();
+  };
+
+  saveData = (blob, filename) => {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.setAttribute("style", "display:none");
+    let url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = filename;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  };
+
+  arrayToCsv(array) {
+    let csvContent = this.state.recordFields.join(",") + "\r\n";
+    csvContent += array
+      .map((e) => {
+        let csv =
+          this.state.recordFields
+            .map((r) => {
+              return e[r];
+            })
+            .join(",") + "\r\n";
+        // console.log(csv);
+        // csv = csv.substr(0, csv.length - 1) + "\r\n";
+        return csv;
+      })
+      .join("\n");
+    return csvContent;
+  }
+
+  createZipFile = () => {
+    let categories = this.state.categories.filter((c) => c.checked);
+    if (categories.length === 0) {
+      this.setState({
+        status:
+          "No categories selected. Please select a cateogory before clicking download",
+      });
+      this.setState({
+        alertLevel: "danger",
+      });
+      return;
+    }
+
+    let zip = new JSZip();
+    let date = new Date();
+    let d = date.toDateString();
+    let t = date.toLocaleTimeString().replace(/:/gm, "_");
+    categories.map((c) => {
+      zip.file(
+        `${c.name}_${d}-${t}.csv`,
+        this.arrayToCsv(this.state.records[c.name])
+      );
+      return c;
+    });
+    zip.generateAsync({ type: "blob" }).then((content) => {
+      this.saveData(content, `health_data_${d}-${t}.zip`);
+    });
+  };
+
+  readBlob = (file) => {
+    let start = 0;
+    let stop = file.size - 1;
+    let reader = new FileReader();
+    reader.onloadend = (evt) => {
+      if (evt.target.readyState === FileReader.DONE) {
+        this.setState({ fileContents: evt.target.result });
+        var convert = require("xml-js");
+        let result = convert.xml2json(evt.target.result, {
+          compact: true,
+          spaces: 4,
+        });
+        const records = this.state.records;
+        const categories = this.state.categories;
+        JSON.parse(result).HealthData.Record.map((r) => {
+          const regex = /HK.*TypeIdentifier/i;
+          let key = r._attributes.type.replace(regex, "");
+          if (!(key in this.state.records)) {
+            records[key] = [];
+            categories.push({ name: key, checked: false });
+          }
+          records[key].push(r._attributes);
+          return r;
+        });
+        this.setState({ records: records });
+        this.setState({ categories });
+        this.setState({
+          healthData: JSON.parse(result).HealthData.Record,
+        });
+        this.setState({ status: <strong>Parsing data complete.</strong> });
+        this.setState({ alertLevel: "success" });
+      }
+    };
+    var blob = file.slice(start, stop + 1);
+    reader.readAsBinaryString(blob);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div className="jumbotron">
+            <div className="container">
+              <div className="row mt-5">
+                <div className="col">
+                  <h1 className="display-4">iOS Health Data Parser</h1>
+                  <p className="lead">
+                    This is application parses the XML data exported from iOS
+                    Health app and creates CSV files for the chosen health
+                    categories.
+                  </p>
+                  <hr className="my-4" />
+                  <p>
+                    This application was developed to support research funded by
+                    Northumbria University and the European Regional Development
+                    Fund’s Intensive Industrial Innovation Programme as part of
+                    doctoral PhD programme. The sponsoring Small to Medium
+                    Enterprise for this programme was Ryder Architecture and it
+                    was delivered through Northumbria University.
+                  </p>
+                  <div className="container">
+                    <div className="row my-4">
+                      <div className="col-4">
+                        <a
+                          target="_blank"
+                          href="https://ec.europa.eu/regional_policy/en/funding/erdf/"
+                          rel="noreferrer noopener"
+                        >
+                          <img
+                            src={erdf}
+                            alt="European Regional Development Fund logo"
+                          />
+                        </a>
+                      </div>
+                      <div className="col-4">
+                        <a
+                          target="_blank"
+                          href="https://www.northumbria.ac.uk/"
+                          rel="noreferrer noopener"
+                        >
+                          <img
+                            src={northumbria}
+                            alt="Northumbria University Logo"
+                          />
+                        </a>
+                      </div>
+                      <div className="col-4">
+                        <a
+                          target="_blank"
+                          href="https://ryderarchitecture.com"
+                          rel="noreferrer noopener"
+                        >
+                          <img src={ryder} alt="Ryder Architecture Logo" />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <hr className="my-4" />
+                  <h5>Data Privacy Statement</h5>
+                  <p>
+                    This app works client-side (processed by your browser) and
+                    no data is collected, stored or managed by this application.
+                    When the Save File(s) button is clicked a file is generated
+                    locally in your browser and will automatically download to
+                    your machine as a compressed archive.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <p
+                className={`alert alert-${this.state.alertLevel}`}
+                id="file_name"
+              >
+                {this.state.status}
+              </p>
+              <div
+                id="drop_zone"
+                onDragOver={(e) => this.handleOnDragOver(e)}
+                onDrop={(e) => this.handleOnDrop(e)}
+              >
+                <p>DROP iOS Health export.xml FILE&nbsp;HERE</p>
+              </div>
+            </div>
+          </div>
+          <div className="row mt-4">
+            <div className="col">
+              <div className="alert alert-warning" role="alert">
+                After the file has been parsed, a list of health categories will
+                be generated below. From this list, select the categories you
+                wish export then press the export button
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-8">
+              <ul id="category">
+                {this.state.categories.map((c) => (
+                  <React.Fragment key={`${c.name}_fragment`}>
+                    <li key={c.name}>
+                      <div
+                        key={`${c.name}_switch`}
+                        className="form-check form-switch"
+                      >
+                        <input
+                          key={`${c.name}_input`}
+                          className="form-check-input"
+                          type="checkbox"
+                          id={`${c.name}_flexSwitchCheckDefault`}
+                          checked={c.checked}
+                          onChange={(e) => this.handleOnCheck(e, c.name)}
+                        />
+                        <label
+                          key={`${c.name}_label+`}
+                          className="form-check-label"
+                          htmlFor={`${c.name}_flexSwitchCheckDefault`}
+                        >
+                          {c.name}
+                        </label>
+                      </div>
+                    </li>
+                  </React.Fragment>
+                ))}
+              </ul>
+            </div>
+            <div className="col-4">
+              {this.state.categories.length > 0 ? (
+                <div key="download_btn" className="d-grid gap-2">
+                  <button
+                    className="btn btn-primary btn-lg"
+                    type="button"
+                    onClick={() => this.handleDownloadClick()}
+                  >
+                    <i className="fa fa-download"></i>&nbsp;Save File(s)
+                  </button>
+                </div>
+              ) : (
+                React.Fragment
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
